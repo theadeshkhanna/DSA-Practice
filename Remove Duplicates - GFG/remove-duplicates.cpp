c6179b59a20adaa2c8e2f17c96d22dc25235f6cb
Adesh Khanna <adeshkhanna271@gmail.com>
2022-03-06 00:45:02 +0530

    Added solution - LeetHub

---
+++ Remove Duplicates - GFG/remove-duplicates.cpp
@@ -0,0 +1,55 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+//User function template for C++
+class Solution{
+public:	
+		
+	string removeDups(string s) 
+	{
+	    map<char, int> m;
+	    int n = s.size();
+	    string ans = "";
+	    
+	    for (int i = 0; i < n; i++) {
+	        if (m.count(s[i]) > 0) {
+	            continue;
+	        } else {
+	            ans += s[i];
+	            m[s[i]]++;
+	        }
+	    }
+	    
+	    return ans;
+	}
+};
+
+// { Driver Code Starts.
+
+
+int main() 
+{
+   	
+
+   	ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+    cout.tie(NULL);
+   
+   	int t;
+   	cin >> t;
+   	while(t--)
+   	{
+   		string s;
+   		cin >> s;
+
+   		
+   		Solution ob;
+   		cout << ob.removeDups(s) << "\n";
+   		
+   	}
+
+    return 0;
+}  // } Driver Code Ends
