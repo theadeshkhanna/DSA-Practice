6191a85ea093188afffd5af41e9e476b39fca221
Adesh Khanna <adeshkhanna271@gmail.com>
2022-04-03 01:02:42 +0530

    Added solution - LeetHub

---
+++ Intersection of Two Linked Lists - GFG/intersection-of-two-linked-lists.cpp
@@ -0,0 +1,120 @@
+// { Driver Code Starts
+//
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node *next;
+    Node(int val)
+    {
+        data=val;
+        next=NULL;
+    }
+};
+
+Node* inputList(int size)
+{
+    Node *head, *tail;
+    int val;
+    
+    cin>>val;
+    head = tail = new Node(val);
+    
+    while(--size)
+    {
+        cin>>val;
+        tail->next = new Node(val);
+        tail = tail->next;
+    }
+    
+    return head;
+}
+
+void printList(Node* n)
+{
+    while(n)
+    {
+        cout<< n->data << " ";
+        n = n->next;
+    }
+}
+
+
+ // } Driver Code Ends
+/* structure of list node:
+
+struct Node
+{
+    int data;
+    Node *next;
+    Node(int val)
+    {
+        data=val;
+        next=NULL;
+    }
+};
+
+*/
+
+class Solution{
+  public:
+    Node* findIntersection(Node* head1, Node* head2)
+    {
+       Node *p = head2 ;
+       Node *q = head1 ;
+       Node *ans = new Node(0) ;
+       Node *head = ans ;
+       
+       set<int>s ;
+       
+       while(p!=NULL)
+       {
+           s.insert(p->data) ;
+           p = p->next ;
+       }
+       
+       while(q!=NULL)
+       {
+           auto it = s.find(q->data) ;
+           if(it != s.end())
+           {
+               Node *temp = new Node(q->data) ;
+               ans->next = temp ;
+               ans = temp ;
+           }
+           
+           q = q->next ;
+       }
+       
+       return head->next ;
+   
+    }
+};
+
+// { Driver Code Starts.
+
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n,m;
+	    
+	    cin>> n;
+	    Node* head1 = inputList(n);
+	    
+	    cin>>m;
+	    Node* head2 = inputList(m);
+	    Solution obj;
+	    Node* result = obj.findIntersection(head1, head2);
+	    
+	    printList(result);
+	    cout<< endl;
+	}
+	return 0;
+}
+  // } Driver Code Ends
