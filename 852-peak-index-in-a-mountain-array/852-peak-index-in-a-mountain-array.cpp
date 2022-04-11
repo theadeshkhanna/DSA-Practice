b3a520c90d9de67a6a3a426d53a551184785a494
Adesh Khanna <adeshkhanna271@gmail.com>
2022-04-12 01:31:19 +0530

    Time: 16 ms (39.93%), Space: 11.6 MB (50.15%) - LeetHub

--- 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.cpp
+++ 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.cpp
@@ -1,12 +1,40 @@
 class Solution {
 public:
-    int peakIndexInMountainArray(vector<int>& arr) {
-        int elem = *max_element(arr.begin(), arr.end());;
+    int peakIndexInMountainArray(vector<int>& nums) {
+        int n = nums.size();
         
-        for (int i = 0; i < arr.size(); i++) {
-            if (arr[i] == elem) return i;
+        if (n == 1) return 0;
+        if (n == 2) return nums[0] > nums[1] ? 0 : 1;
+        
+        int lo = 0;
+        int hi = n - 1;
+        
+        while(lo <= hi) {
+            int mid = lo + (hi - lo) / 2;
+            
+            if (mid > 0 && mid < n - 1) {
+                if (nums[mid] > nums[mid - 1] && nums[mid] > nums[mid + 1]) {
+                    return mid;
+                } else if (nums[mid] > nums[mid - 1]) {
+                    lo = mid + 1;
+                } else {
+                    hi = mid - 1;
+                }
+            } else if (mid == 0) {
+                if (nums[0] > nums[1]) {
+                    return 0;
+                } else {
+                    return 1;
+                }
+            } else if (mid == n - 1) {
+                if (nums[n - 1] > nums[n - 2]) {
+                    return n - 1;
+                } else {
+                    return n - 2;
+                }
+            }
         }
         
-        return 0;
+        return -1;
     }
 };
