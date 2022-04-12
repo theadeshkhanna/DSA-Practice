3f5153074a051ab260923dcf13da6acf3f1ec675
Adesh Khanna <adeshkhanna271@gmail.com>
2022-04-12 23:06:44 +0530

    Time: 0 ms (100.00%), Space: 7.1 MB (20.27%) - LeetHub

---
+++ 289-game-of-life/289-game-of-life.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    bool isValid(int x, int y, int n, int m) {
+        if (x < 0 || x >= n || y < 0 || y >= m) return false;
+        else return true;
+    }
+    
+    void gameOfLife(vector<vector<int>>& board) {
+        int n = board.size();
+        int m = board[0].size();
+        
+        vector<int> dx = {0, 0, 1, 1, 1, -1, -1, -1};
+        vector<int> dy = {1, -1, 1, -1, 0, 0, 1, -1};
+        
+        vector<vector<int>>ans(n,vector<int>(m,0));
+        
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                int live = 0;
+                
+                for (int k = 0; k < 8; k++) {
+                    int cx = i + dx[k];
+                    int cy = j + dy[k];
+                    
+                    if (isValid(cx, cy, n, m) && board[cx][cy] == 1) live++;
+                }
+                
+                if (board[i][j] == 1){
+                    if (live < 2 || live > 3) ans[i][j] = 0;
+                    else ans[i][j] = 1;
+                } else {
+                    if (live == 3) ans[i][j] = 1;
+                    else ans[i][j] = 0;
+                }
+            }
+        }
+        
+        board = ans;
+    }
+};
