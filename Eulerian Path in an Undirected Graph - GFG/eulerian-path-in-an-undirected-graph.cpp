27f5e64a15df3a5b8e4e70b2420f559405e859ed
Adesh Khanna <adeshkhanna271@gmail.com>
2022-05-04 00:57:42 +0530

    Added solution - LeetHub

---
+++ Eulerian Path in an Undirected Graph - GFG/eulerian-path-in-an-undirected-graph.cpp
@@ -0,0 +1,45 @@
+// { Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+public:
+    int eulerPath(int N, vector<vector<int>> graph) {
+        int ans = 0;
+        for (int i = 0; i < N; i++) {
+            int cnt = 0;
+            for (int j = 0; j < graph[i].size(); j++) {
+                if (graph[i][j]) cnt++;
+            }
+            
+            if (cnt % 2 == 1) ans++;
+        }
+        
+        if (ans == 0 || ans == 2) return 1;
+        else return 0;
+    }
+};
+
+// { Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int N;
+        cin>>N;
+        vector<vector<int>> graph(N, vector<int>(N, -1));
+        for(int i = 0;i < N;i++)
+            for(int j = 0;j < N;j++)
+                cin>>graph[i][j];
+        
+        Solution ob;
+        cout<<ob.eulerPath(N, graph)<<"\n";
+    }
+    return 0;
+}  // } Driver Code Ends
