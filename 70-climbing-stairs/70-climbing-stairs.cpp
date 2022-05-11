61b431c95a8c78628c61455077fd37dfa581604e
Adesh Khanna <adeshkhanna271@gmail.com>
2022-05-11 22:43:54 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (56.39%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -2,12 +2,12 @@ class Solution {
 public:
     int climbStairs(int n) {
         if (n <= 2) return n;
-        int prev2 = 1, prev = 2, ans;
+        int last2 = 1, last = 2, ans;
         
         for (int i = 3; i <= n; i++) {
-            ans = prev + prev2;
-            prev2 = prev;
-            prev = ans;
+            ans = last + last2;
+            last2 = last;
+            last = ans;
         }
         
         return ans;
