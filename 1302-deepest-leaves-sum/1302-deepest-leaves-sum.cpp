3b434191ab8f0ff333bfe4a9f532961821a818c8
Adesh Khanna <adeshkhanna271@gmail.com>
2022-05-15 14:10:22 +0530

    Time: 114 ms (75.53%), Space: 61.7 MB (43.30%) - LeetHub

--- 1302-deepest-leaves-sum/1302-deepest-leaves-sum.cpp
+++ 1302-deepest-leaves-sum/1302-deepest-leaves-sum.cpp
@@ -12,37 +12,25 @@
 class Solution {
 public:
     int deepestLeavesSum(TreeNode* root) {
-        int h = height(root);
         int sum = 0;
-        
         queue<TreeNode*> q;
         q.push(root);
-        int level = 1;
         
         while(q.size() > 0) {
             int s = q.size();
+            sum = 0;
             
             for (int i = 0; i < s; i++) {
                 TreeNode* fr = q.front();
                 q.pop();
                 
-                if (level == h) sum += fr->val;
+                sum += fr->val;
                 
                 if (fr->left) q.push(fr->left);
                 if (fr->right) q.push(fr->right);
-            }
-            
-            level++;
+            }            
         }
         
         return sum;
-    }
-    
-    int height(TreeNode* root) {
-        if (root == NULL) {
-            return 0;
-        }
-        
-        return 1 + max(height(root->left), height(root->right));
     }
 };
