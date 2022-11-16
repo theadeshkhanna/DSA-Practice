ec56284139ea39f03bcb434d054cbdebf4558098
Adesh Khanna <adeshkhanna271@gmail.com>
2022-11-16 14:23:07 +0530

    Added solution - LeetHub

---
+++ Find n/k th node in Linked list - GFG/find-nk-th-node-in-linked-list.cpp
@@ -0,0 +1,92 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+int fractional_node(struct Node* head,int k);
+
+struct Node
+{
+    int data;
+    struct Node *next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+    
+};
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        Node* ptr,*start = NULL,*ptr1;
+        int n,i;
+        cin>>n;
+        
+        for(i=0;i<n;i++)
+        {
+            int value;
+            cin>>value;
+            
+            ptr=new Node(value);
+            
+            if(start==NULL)
+            {
+                start=ptr;
+                ptr1=ptr;
+            }
+            else
+            {
+                ptr1->next=ptr;
+                ptr1=ptr1->next;
+            }
+        }
+        ptr1->next=NULL;
+        int k;
+        cin>>k;
+        int p = fractional_node(start,k);
+        cout<<p<<endl;
+    }
+}
+
+// } Driver Code Ends
+
+
+/*The structure of the node is
+struct Node
+{
+    int data;
+    struct Node *next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+    
+};
+*/
+int fractional_node(struct Node *head, int k)
+{
+    Node* temp = head;
+    int count = 0;
+    
+    while(temp) {
+        temp = temp->next;
+        count++;
+    }
+    
+    int idx = ceil((float)count / (float)k);
+    
+    Node* ex = head;
+    int i = 1;
+    
+    while(ex && i < idx) {
+        ex = ex->next;
+        i++;
+    }
+    
+    return ex->data;
+}
