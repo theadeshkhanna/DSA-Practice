c42f5a44544324655dbaec37ca7e35885f9128b0
Adesh Khanna <adeshkhanna271@gmail.com>
2023-03-14 23:21:12 +0530

    Added solution - LeetHub

---
+++ Pattern - GFG/pattern.cpp
@@ -0,0 +1,58 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    void printDiamond(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                cout << " ";
+            }
+            
+            for (int j = 0; j < 2 * i + 1; j++) {
+                if (j % 2 != 0) {
+                    cout << " ";
+                } else {
+                    cout << "*";
+                }
+            }
+            
+            cout << endl;
+        }
+        
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = 0; j < n - i - 1; j++) {
+                cout << " ";
+            }
+            
+            for (int j = 0; j < 2 * i + 1; j++) {
+                if (j % 2 != 0) {
+                    cout << " ";
+                } else {
+                    cout << "*";
+                }
+            }
+            
+            cout << endl;
+        }
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+
+        Solution ob;
+        ob.printDiamond(n);
+    }
+    return 0;
+}
+// } Driver Code Ends
