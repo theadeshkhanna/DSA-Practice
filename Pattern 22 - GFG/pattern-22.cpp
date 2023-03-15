7e854037423b1d92acb7c29882733e94aba948f1
Adesh Khanna <adeshkhanna271@gmail.com>
2023-03-15 22:18:52 +0530

    Added solution - LeetHub

---
+++ Pattern 22 - GFG/pattern-22.cpp
@@ -0,0 +1,35 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    void printSquare(int n) {
+        for (int i = 0; i < 2 * n - 1; i++) {
+            for (int j = 0; j < 2 * n - 1; j++) {
+                cout << n - (
+                    min(i, (min(j, min(2 * n - 2 - j, 2 * n - 2 - i))))    
+                ) << " ";
+            }
+            cout << endl;
+        }
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+
+        Solution ob;
+        ob.printSquare(n);
+    }
+    return 0;
+}
+// } Driver Code Ends
