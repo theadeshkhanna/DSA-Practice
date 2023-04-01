48b7c3b421423050d6e0798619525d77023e4adf
Adesh Khanna <adeshkhanna271@gmail.com>
2023-04-01 13:23:38 +0530

    Added solution - LeetHub

--- Longest consecutive subsequence - GFG/longest-consecutive-subsequence.cpp
+++ Longest consecutive subsequence - GFG/longest-consecutive-subsequence.cpp
@@ -1,43 +1,41 @@
-// { Driver Code Starts
+//{ Driver Code Starts
 #include <bits/stdc++.h>
 using namespace std;
 
 
- // } Driver Code Ends
+// } Driver Code Ends
 class Solution{
   public:
     // arr[] : the input array
     // N : size of the array arr[]
     
     //Function to return length of longest subsequence of consecutive integers.
-    int findLongestConseqSubseq(int nums[], int n)
+    int findLongestConseqSubseq(int arr[], int N)
     {
-        int finalLength = 0;
-        unordered_set<int> s;
-        
-        for (int i = 0; i < n; i++) {
-            s.insert(nums[i]);
-        }
-        
-        for (int i = 0; i < n; i++) {
-            if (!s.count(nums[i] - 1)) {
-                int elem = nums[i];
-                int length = 1;
-                
-                while(s.count(elem + 1)) {
-                    elem += 1;
-                    length += 1;
-                }
-                
-                finalLength = max(finalLength, length);
-            }
-        }
-        
-        return finalLength;
+      int longest = INT_MIN;
+      unordered_set<int> st;
+      
+      for (int i = 0; i < N; i++) {
+          st.insert(arr[i]);
+      }
+      
+      for (auto it: st) {
+          if (st.find(it - 1) == st.end()) {
+              int cnt = 1;
+              int x = it;
+              while(st.find(x + 1) != st.end()) {
+                  x = x + 1;
+                  cnt = cnt + 1;
+              }
+              longest = max(longest, cnt);
+          }
+      }
+      
+      return longest;
     }
 };
 
-// { Driver Code Starts.
+//{ Driver Code Starts.
  
 // Driver program
 int main()
@@ -54,4 +52,5 @@ int main()
  }
       
     return 0;
-}  // } Driver Code Ends
+}
+// } Driver Code Ends
